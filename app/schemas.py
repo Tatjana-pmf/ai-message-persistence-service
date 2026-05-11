76874b3d82dfa2c840db718161d480d19f15cded
Tatjana-pmf <tatjana_1995@hotmail.com>
2026-05-11 19:39:42 +0200

    add message shemas

---
+++ app/schemas.py
@@ -0,0 +1,31 @@
+from datetime import datetime
+from uuid import UUID
+
+from pydantic import BaseModel, ConfigDict
+
+from app.models import MessageRole
+
+
+class MessageCreate(BaseModel):
+    message_id: UUID | None = None
+    chat_id: UUID
+    content: str
+    rating: bool | None = None
+    sent_at: datetime
+    role: MessageRole
+
+
+class MessageUpdate(BaseModel):
+    content: str | None = None
+    rating: bool | None = None
+
+
+class MessageResponse(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+
+    message_id: UUID
+    chat_id: UUID
+    content: str
+    rating: bool | None
+    sent_at: datetime
+    role: MessageRole
